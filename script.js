039810926853fc12592aa534080d619103566ab3
Dragosh51 <dragosh.bindila@gmail.com>
2023-11-19 17:28:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('skills').addEventListener('click', function() {
+    // Navigate to the Skills page or handle accordingly
+    console.log('Navigating to Skills page');
+});
+
+document.getElementById('contact').addEventListener('click', function() {
+    // Navigate to the Contact page or handle accordingly
+    console.log('Navigating to Contact page');
+});
+
+document.getElementById('fun-facts').addEventListener('click', function() {
+    // Navigate to the Fun Facts page or handle accordingly
+    console.log('Navigating to Fun Facts page');
+});
